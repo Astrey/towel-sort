ce567792e6f2fb0961c953facf70fcd085cb3284
Anna Glukhova <ann.astrey@gmail.com>
2022-07-14 00:57:51 +0300

    feat: towel sort added

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let towel = []
+  matrix.forEach((element, index) => {
+    towel.push(element.sort((a, b) => ((index%2==0)?(a-b):(b-a))));
+  });
+  return towel.join().split(',').map(v=>v*1);
 }
+
+module.exports([
+  [ 1, 2, 3 ],
+  [ 4, 5, 6 ],
+  [ 7, 8, 9 ],
+ ])
