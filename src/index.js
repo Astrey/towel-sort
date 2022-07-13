1b6fe29f53a405b67800bef95e9c56959c3767a2
Anna Glukhova <ann.astrey@gmail.com>
2022-07-14 01:03:26 +0300

    feat: towel sort fixed

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+  if (!matrix) {
+    return [];
+  }
   let towel = []
   matrix.forEach((element, index) => {
     towel.push(element.sort((a, b) => ((index%2==0)?(a-b):(b-a))));
   });
-  return towel.join().split(',').map(v=>v*1);
+  return (matrix.length > 0 && matrix)?(towel.join().split(',').map(v=>v*1)):[];
 }
-
-module.exports([
-  [ 1, 2, 3 ],
-  [ 4, 5, 6 ],
-  [ 7, 8, 9 ],
- ])
